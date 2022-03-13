29123c02481f4eef16f6ec40e8ecbb6ba2694054
HectorMRC <thehector1593@gmail.com>
2022-03-13 19:02:23 +0100

    architecture refactoring

--- ingress/volumes/lua/security.lua
+++ ingress/volumes/lua/security.lua
@@ -1,16 +1,51 @@
-local verify_token = function(self)
-    return ''
-end
-
-local methods = {
-    verify_token = verify_token,
+local _M = {
+    http = require("resty.http")
 }
 
-local new = function()
-    local instance = setmetatable({}, {__index = methods})
+function _M:verify_token(token)
+    if token == nil then
+        return {
+            valid=false,
+            error="token not found"
+        }
+    end
+
+    local res, err = self.http.new():request_uri(self.url, {
+        method = "GET",
+        headers = {
+            [self.target_header] = token,
+        },
+    })
+
+    if not res then
+        return {
+            valid=false,
+            error="request failed: "..err
+        }
+    end
+
+    if res.status ~= 200 then
+        return {
+            valid=false,
+            error="status: "..res.status
+        }
+    end
+
+    return {
+        valid=true,
+        data=res.body,
+    }
+end
+
+function _M:new(url, target_header)
+    local instance = {
+        url = url,
+        target_header = target_header,
+    }
+    
+    setmetatable(instance, self)
+    self.__index = self
     return instance
 end
 
-return {
-    new = new,
-}
+return _M
--- rauth/volumes/lua/security.lua
+++ rauth/volumes/lua/security.lua
@@ -1,129 +1,109 @@
-local verify_token = function(self)
-    local jwt_token = ngx.req.get_headers()[self.jwt_header]
-    if jwt_token == nil then
-        ngx.status = 404
-        ngx.say('token not found')
-        ngx.exit(ngx.OK)
-        return
-    end
+local _M = {
+    b64 = require("ngx.base64"),
+    jwt = require("resty.jwt"),
+    redis = require("resty.redis")
+}
 
-    local jwt_token, err = self.b64.decode_base64url(self.jwt_token)
-    if not jwt_token then
-        ngx.status = 400
-        ngx.say('could not decode token: ', err)
-        ngx.exit(ngx.OK)
-        return
-    end
-
-    local jwt_public, err = self.b64.decode_base64url(self.jwt_public)
-    if not jwt_public then
-        ngx.status = 400
-        ngx.say('could not decode public key: ', err)
-        ngx.exit(ngx.OK)
-        return
-    end
-
-    local claims = self.jwt:verify(self.jwt_public, self.jwt_token)
-    local verified = claims['verified']
-
-    if not verified then
-        local reason = claims['reason']
-        ngx.status = 498
-        ngx.say('unverified token: ', reason)
-        ngx.exit(ngx.OK)
-        return
-    end
-
-    local payload = claims['payload']
-    if payload == nil then 
-        ngx.status = 498
-        ngx.say('no payload found')
-        ngx.exit(ngx.OK)
-        return
-    end
-
-    local jti = payload['jti']
-    if jti == nil then 
-        ngx.status = 498
-        ngx.say('no token id (jti) in payload')
-        ngx.exit(ngx.OK)
-        return
-    end
-
-    ngx.var.subject = payload['sub']
-    if ngx.var.subject == nil then 
-        ngx.status = 498
-        ngx.say('no subject (sub) in payload')
-        ngx.exit(ngx.OK)
-        return
-    end
-
+function _M:look_up_token_id(jti)
     local red = self.redis:new()
     red:set_timeouts(1000, 1000, 1000)
 
     local ok, err = red:connect(self.redis_dsn, 6379)
     if not ok then
-        ngx.status = 500
-        ngx.say('failed to connect to redis cluster: ', err)
-        ngx.exit(ngx.OK)
-        return
+        return nil, err
     end
 
     local res, err = red:get(jti)
     if not res then
-        ngx.status = 498
-        ngx.say('failed to get token with id ', jti, ': ', err)
-        ngx.exit(ngx.OK)
-        return
+        return nil, err
     end
 
     if res == ngx.null then
-        ngx.status = 498
-        ngx.say('failed to get token with id ', jti, ': not found')
-        ngx.exit(ngx.OK)
-        return
+        return nil, "not found"
     end
 
     local ok, err = red:set_keepalive(10000, 100)
     if not ok then
-        ngx.status = 200
-        ngx.say('failed to set keepalive: ', err)
-        ngx.exit(ngx.OK)
-        return
+        ngx.log(ngx.ERR, "failed to keepalive redis connection: ", err)
     end
 
-    ngx.status = 200
-    ngx.exit(ngx.OK)
+    return res, nil
 end
 
-local methods = {
-    verify_token = verify_token,
-}
-
-local new = function(redis_dsn, jwt_public, jwt_header)
-    local instance = setmetatable({}, {__index = methods})
-    instance.b64 = require("ngx.base64")
-    instance.jwt = require("resty.jwt")
-    instance.redis = require("resty.redis")
-    instance.redis_dsn = redis_dsn
-    instance.jwt_public = jwt_public
-    if jwt_header == nil then
-        instance.jwt_header  = "authorization"
-    else
-        instance.jwt_header = jwt_header
+function _M:verify_token(token)
+    local jwt_token, err = self.b64.decode_base64url(token)
+    if not jwt_token then
+        return {
+            status=400,
+            error="could not decode token: "..err
+        }
     end
 
+    -- local public_key, err = self.b64.decode_base64url(self.public_key)
+    -- if not public_key then
+    --     return {
+    --         status=500,
+    --         error="could not decode public key: "..err
+    --     }
+    -- end
+
+    -- local claims = self.jwt:verify(self.public_key, jwt_token)
+    -- local verified = claims['verified']
+
+    -- if not verified then
+    --     return {
+    --         status=498,
+    --         error="unverified token: "..claims['reason']
+    --     }
+    -- end
+
+    -- local payload = claims['payload']
+    -- if payload == nil then 
+    --     return {
+    --         status=498,
+    --         error="no payload found"
+    --     }
+    -- end
+
+    -- local jti = payload['jti']
+    -- if jti == nil then 
+    --     return {
+    --         status=498,
+    --         error="no token id (jti) in payload"
+    --     }
+    -- end
+
+    -- local redis_res, err = self.look_up_token_id(jti)
+    -- if redis_res == nil then
+    --     return {
+    --         status=498,
+    --         error="token is not valid: "..err
+    --     }
+    -- end
+
+    -- local user_id = payload['sub']
+    -- if user_id == nil then 
+    --     return {
+    --         status=498,
+    --         error="no subject (sub) in payload"
+    --     }
+    -- end
+
+    return {
+        status=200
+    }
+
+end
+
+function _M:new(redis_dsn, public_key)
+    local instance = {
+        redis_dsn = redis_dsn,
+        public_key = public_key,
+    }
+    
+    setmetatable(instance, self)
+    self.__index = self
     return instance
 end
 
-local from_environment = function()
-    local redis_dsn = os.getenv("REDIS_DSN")
-    local jwt_public = os.getenv("JWT_PUBLIC")
-    local jwt_header = os.getenv("JWT_HEADER")
-    return new(redis_dsn, jwt_public, jwt_header)
-end
-
-return {
-    new = new,
-    from_environment = from_environment,
-}
+return _M
